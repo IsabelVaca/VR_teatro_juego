3cbf1d691c0535f4d1de2ab094385a13f184f240
Isabel Vaca <A01352522@tec.mx>
2026-06-03 00:58:43 -0600

    fer

--- Assets/Scripts/ColorButton.cs
+++ Assets/Scripts/ColorButton.cs
@@ -3,12 +3,53 @@ using UnityEngine.Events;
 
 public class ColorButton : MonoBehaviour
 {
+    [Header("Color del botón")]
     public string colorName;
+
+    [Header("Evento al activar botón")]
     public UnityEvent<string> onButtonPressed;
+
+    [Header("Materiales")]
+    public Renderer buttonRenderer;
+    public Material normalMaterial;
+    public Material hoverMaterial;
+
+    [Header("Evitar que se active muchas veces")]
+    public float cooldown = 1f;
+    private bool canPress = true;
+
+    public void OnHoverEnter()
+    {
+        if (buttonRenderer != null && hoverMaterial != null)
+        {
+            buttonRenderer.material = hoverMaterial;
+        }
+
+        PressButton();
+    }
+
+    public void OnHoverExit()
+    {
+        if (buttonRenderer != null && normalMaterial != null)
+        {
+            buttonRenderer.material = normalMaterial;
+        }
+    }
 
     public void PressButton()
     {
+        if (!canPress) return;
+
+        canPress = false;
+
         Debug.Log("Presionaste: " + colorName);
         onButtonPressed.Invoke(colorName);
+
+        Invoke(nameof(ResetButton), cooldown);
+    }
+
+    private void ResetButton()
+    {
+        canPress = true;
     }
 }
